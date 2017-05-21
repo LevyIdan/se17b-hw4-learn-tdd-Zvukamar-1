bb0578dbbc83c7c825e095bc74ce4847972f7f7c
Zvukamar <fre3run@gmail.com>
2017-05-21 12:15:03 +0200

    REFACTOR - 100% Coverage for Statements, Branches, Functions and Lines.

--- cash.js
+++ cash.js
@@ -1,3 +1,4 @@
+// 100% Coverage for Statements, Branches, Functions and Lines.
 var C = {};     // C Object simplifies exporting the module
 C.coins = [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
 C.getChange = function (totalPayable, cashPaid) {
